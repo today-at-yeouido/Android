77c3018deed5a8759a81325a68774a5021cdeddd
Liv66 <rlawjdgjs73@gmail.com>
2022-07-18 23:42:27 +0900

    feat: create BottomBar UI
    create BottomBar UI
    - material icon extension
    - use BottomNavigation Layout, BottomNavigationItem
    
    - have to replace Custom
    
    #2

--- TAYApp/app/src/main/java/com/example/tayapp/presentation/home/HomeScreen.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/home/HomeScreen.kt
@@ -1,14 +1,22 @@
 package com.example.tayapp.presentation.home
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import com.example.tayapp.presentation.components.BottomBarTabs
 
 @Composable
 fun HomeScreen() {
-    Text(text = "HI", modifier = Modifier.fillMaxSize(), fontSize = 60.sp)
+    Column(modifier = Modifier.fillMaxSize()) {
+        Icon(imageVector = BottomBarTabs.HOME.icon, contentDescription = "")
+        Icon(imageVector = BottomBarTabs.SCRAP.icon, contentDescription = "")
+        Icon(imageVector = BottomBarTabs.REPORT.icon, contentDescription = "")
+        Icon(imageVector = BottomBarTabs.PROFILE.icon, contentDescription = "")
+    }
 }
