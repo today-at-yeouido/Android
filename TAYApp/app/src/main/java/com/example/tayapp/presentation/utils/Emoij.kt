62645096695226066f05ab9f9a8bd06892b0604c
GaheeKim <aqswefr@naver.com>
2022-07-25 14:36:35 +0900

    feature: Emoij add
    add Emoij file and TagAppBar modify

---
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/utils/Emoij.kt
@@ -0,0 +1,23 @@
+package com.example.tayapp.presentation.utils
+
+val Emoij = mapOf(
+    "행정안전" to "\uD83D\uDEA8",
+    "보건복지" to "\uD83D\uDC89",
+    "국토교통" to "\uD83D\uDEE3",
+    "기획재정" to "\uD83E\uDE99",
+    "환경" to "\uD83C\uDF33",
+    "노동" to "\uD83D\uDC77\uD83C\uDFFB",
+    "교육" to "\uD83D\uDCDA",
+    "사법" to "⚖️",
+    "문화체육관광" to "\uD83C\uDF88",
+    "정무" to "\uD83D\uDCBC",
+    "농림" to "\uD83C\uDF3E",
+    "해양" to "\uD83C\uDF0A",
+    "과학통신" to "\uD83D\uDD2C",
+    "산업기업" to "\uD83C\uDFE2",
+    "정보" to "\uD83D\uDCD1",
+    "외교통일" to "\uD83C\uDF0F",
+    "여성가족" to "\uD83E\uDEC2",
+    "국방" to "\uD83E\uDE96",
+    "국회운영" to "\uD83D\uDCE3"
+)
