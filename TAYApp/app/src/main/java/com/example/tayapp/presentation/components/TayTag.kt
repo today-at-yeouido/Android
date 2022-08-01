43e1b3a0ff77b90fb903b8945b4e95af0f413fa0
GaheeKim <aqswefr@naver.com>
2022-08-01 12:38:18 +0900

    feature: Create TayTag and Delete HomeTag
    Create TayTag and Delete HomeTag

---
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/components/TayTag.kt
@@ -0,0 +1,46 @@
+package com.example.tayapp.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tayapp.presentation.ui.theme.TayAppTheme
+
+@Composable
+fun TayTag(
+    string: String = "전체",
+    isClicked: Boolean = false,
+    onClick:(String)->Unit = {}
+){
+    Box(
+        modifier = Modifier
+            .background(
+                color = if (isClicked) TayAppTheme.colors.icon
+                else TayAppTheme.colors.background,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = TayAppTheme.colors.border,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .padding(
+                horizontal = 10.dp,
+                vertical = 5.dp
+            )
+            .clickable { onClick(string) }
+
+    ) {
+        Text(
+            text = "$string",
+            color = if (isClicked) TayAppTheme.colors.background
+            else TayAppTheme.colors.bodyText
+        )
+    }
+}
