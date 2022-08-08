df5dd33740753984e245bcf749d0f2e7cb7a008b
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2022-08-08 22:54:22 +0900

    Feature/#87 (#88)
    * feature: Profile component
    
    Profile component
    
    * feat: profile component modify
    
    profile component modify
    
    * feat: create Profile screen
    
    create Profile screen
    
    * feature: Profile detail Screen create
    
    Profile detail Screen create
    
    * feature: Profile navigation
    
    Profile navigation

--- TAYApp/app/src/main/java/com/example/tayapp/presentation/navigation/NavDestinations.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/navigation/NavDestinations.kt
@@ -1,5 +1,7 @@
 package com.example.tayapp.presentation.navigation
 
+import android.opengl.Visibility
+
 
 /** Destination 임시로 HOME, DETAIL 설정 */
 object MainDestination {
@@ -16,4 +18,20 @@ object BottomBarDestination {
     const val SEARCH = "home/search"
     const val REPORT = "home/report"
     const val PROFILE = "home/profile"
+}
+
+object ProfileDestination {
+    const val ACCOUNT = "home/profile/account"
+    const val APPSETTING = "home/profile/appsetting"
+    const val INQUIRE = "home/profile/inquire"
+    const val APPINFO = "home/profile/appinfo"
+    const val FAVORITE = "home/profile/account/favorite"
+    const val VISIBILITY = "home/profile/appsetting/visibility"
+    const val ALARM = "home/profile/appsetting/alarm"
+}
+
+object AppGraph {
+    const val PROFILE_GRAPH = "profile_graph"
+    const val PROFILE_ACCOUNT_GRAPH = "profile_account_graph"
+    const val PROFILE_APPSETTING_GRAPH = "profile_appsetting_graph"
 }
---
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/screens/Profile/ProfileVisibility.kt
@@ -0,0 +1,48 @@
+package com.example.tayapp.presentation.screens.Profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material.icons.outlined.FormatSize
+import androidx.compose.material.icons.outlined.LightMode
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tayapp.presentation.components.CardProfileListItemWithLink
+import com.example.tayapp.presentation.components.CardProfileListItemWithNext
+import com.example.tayapp.presentation.components.TayTopAppBarWithBack
+import com.example.tayapp.presentation.ui.theme.KeyLine
+import com.example.tayapp.presentation.ui.theme.TayAppTheme
+
+@Composable
+fun ProfileVisibility(){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TayTopAppBarWithBack(string = "앱 정보")
+        Column(
+            modifier = Modifier.padding(KeyLine)
+        ) {
+            CardProfileListItemWithNext(
+                icon = Icons.Outlined.LightMode,
+                text = "모드",
+                subtext = "시스템(라이트)"
+            )
+            CardProfileListItemWithNext(
+                icon = Icons.Outlined.FormatSize,
+                text = "글자 크기",
+                subtext = "보통"
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewProfileMode(){
+    TayAppTheme {
+        ProfileVisibility()
+    }
+}
