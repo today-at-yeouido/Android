9dc534678258f30dd74f946181730fd7d19ef864
GaheeKim <aqswefr@naver.com>
2022-08-17 16:04:30 +0900

    feat: create billDetail ViewModel
    create billDetail ViewModel

--- TAYApp/app/src/main/java/com/example/tayapp/presentation/viewmodels/DetailViewModel.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/viewmodels/DetailViewModel.kt
@@ -4,14 +4,19 @@ import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.tayapp.domain.use_case.GetBillDetailUseCase
 import com.example.tayapp.domain.use_case.RecentSearchTermUseCase
 import com.example.tayapp.domain.use_case.scrap.PostAddScrapUseCase
 import com.example.tayapp.domain.use_case.scrap.PostDeleteScrapUseCase
 import com.example.tayapp.domain.use_case.search.GetSearchResultUseCase
 import com.example.tayapp.presentation.navigation.Destinations
+import com.example.tayapp.presentation.states.BillDetailUiState
+import com.example.tayapp.presentation.states.SearchState
 import com.example.tayapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,12 +24,42 @@ import javax.inject.Inject
 class DetailViewModel @Inject constructor(
     private val postAddScrapUseCase: PostAddScrapUseCase,
     private val postDeleteScrapUseCase: PostDeleteScrapUseCase,
+    private val getBillDetailUseCase: GetBillDetailUseCase,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel()  {
 
     val billId: Int = savedStateHandle.get<Int>(Destinations.BILL_ID)!!
 
+    var detailState = MutableStateFlow(BillDetailUiState(isLoading = true))
+        private set
 
+    init {
+        getBillDetail(billId)
+    }
+
+    fun getBillDetail(billId: Int){
+        viewModelScope.launch {
+            getBillDetailUseCase(billId).collect(){ result ->
+                when(result){
+                    is Resource.Success -> {
+                        detailState.update {
+                            it.copy(billDetail = result.data, isLoading = false)
+                        }
+                    }
+                    is Resource.Error -> {
+                        detailState.update {
+                            it.copy(error = result.message ?: "An unexpected error", isLoading = false)
+                        }
+                    }
+                    is Resource.Loading -> {
+                        detailState.update {
+                            it.copy(isLoading = true)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     fun addScrap(bill: Int){
         viewModelScope.launch {
