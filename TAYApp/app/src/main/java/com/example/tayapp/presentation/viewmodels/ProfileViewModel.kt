a7a7cb22449aef20b718afaedc8b0ed162997173
Liv66 <rlawjdgjs73@gmail.com>
2022-08-17 15:50:32 +0900

    feature: sns logout logic

--- TAYApp/app/src/main/java/com/example/tayapp/presentation/viewmodels/ProfileViewModel.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/viewmodels/ProfileViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.tayapp.domain.use_case.login.RequestLogoutUseCase
 import com.example.tayapp.presentation.states.LoginState
 import com.example.tayapp.presentation.states.UserInfo
+import com.kakao.sdk.user.UserApiClient
 import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,10 +18,22 @@ class ProfileViewModel @Inject constructor(private val logoutUseCase: RequestLog
 
     fun logout() {
         viewModelScope.launch {
-            Log.d("##99","리프래시 ${LoginState.user.refreshToken}")
             if (logoutUseCase(LoginState.user.refreshToken)) {
+                when (LoginState.user.sns) {
+                    "naver" -> {
+                        NaverIdLoginSDK.logout()
+                    }
+                    "kakao" -> {
+                        UserApiClient.instance.logout { error ->
+                            if (error != null) {
+                                Log.e("##88", "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                            } else {
+                                Log.i("##88", "로그아웃 성공. SDK에서 토큰 삭제됨")
+                            }
+                        }
+                    }
+                }
                 LoginState.user = UserInfo()
-                NaverIdLoginSDK.logout()
             }
         }
     }
