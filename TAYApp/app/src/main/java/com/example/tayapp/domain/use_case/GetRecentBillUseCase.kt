1bdc01370e35a51c224d6dcebdebd3a2979850e1
Liv66 <rlawjdgjs73@gmail.com>
2022-08-17 19:27:27 +0900

    refactor: flow and retryWhen in Domain

--- TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/GetRecentBillUseCase.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/GetRecentBillUseCase.kt
@@ -6,13 +6,12 @@ import androidx.paging.PagingData
 import com.example.tayapp.data.paging_source.RecentBillPagingResource
 import com.example.tayapp.domain.model.Bill
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
 import javax.inject.Inject
 
 class GetRecentBillUseCase
-    @Inject constructor(private val recentBillPagingResource: RecentBillPagingResource) {
-
+@Inject constructor(private val recentBillPagingResource: RecentBillPagingResource) {
     operator fun invoke(): Flow<PagingData<Bill>> = Pager(PagingConfig(pageSize = 1)) {
         recentBillPagingResource
     }.flow
-
 }
--- TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/login/RequestLogoutUseCase.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/login/RequestLogoutUseCase.kt
@@ -4,35 +4,40 @@ import android.util.Log
 import com.example.tayapp.data.remote.dto.login.LogoutResponse
 import com.example.tayapp.data.remote.dto.login.RefreshTokenDto
 import com.example.tayapp.domain.repository.LoginRepository
+import com.example.tayapp.utils.UnAuthorizationError
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.retryWhen
+import retrofit2.HttpException
+import retrofit2.Response
 import javax.inject.Inject
 
 class RequestLogoutUseCase @Inject constructor(
     private val repository: LoginRepository,
     private val checkLoginUseCase: CheckLoginUseCase
 ) {
-
-    suspend operator fun invoke(refToken: String): Boolean {
+    suspend operator fun invoke(refToken: String): Flow<Boolean> = flow {
+        emit(false)
         val r = repository.requestLogout(RefreshTokenDto(refToken))
-        return when (r.code()) {
+        when (r.code()) {
             200 -> {
                 repository.prefLogout()
                 Log.d("##99", "로그아웃 성공 ${r.body()}")
-                true
+                emit(true)
             }
             401 -> {
                 checkLoginUseCase()
-                val c = repository.requestLogout(RefreshTokenDto(refToken))
-                if (c.code() == 200) {
-                    Log.d("##99", "로그아웃 성공 ${r.body()}")
-                    repository.prefLogout()
-                }
-                false
+                throw UnAuthorizationError()
             }
             else -> {
                 Log.d("##99", "로그아웃 ${r.body()}, 코드 ${r.code()}")
-                false
+                emit(false)
             }
         }
+    }.retryWhen { cause, attempt ->
+        Log.d("##88", "404 Error, unAuthorization token")
+        cause is UnAuthorizationError || attempt < 3
     }
-
 }
+
