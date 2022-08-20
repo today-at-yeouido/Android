205d83caa0b0e142d1ee0b7f07e0724195a1a074
Liv66 <rlawjdgjs73@gmail.com>
2022-08-20 17:24:50 +0900

    feature: recent search term duplicate check

--- TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/RecentSearchTermUseCase.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/RecentSearchTermUseCase.kt
@@ -1,12 +1,10 @@
 package com.example.tayapp.domain.use_case
 
 import com.example.tayapp.domain.use_case.recent.GetRecentSearchUseCase
-import com.example.tayapp.domain.use_case.recent.RemoveRecentSearchUseCase
 import com.example.tayapp.domain.use_case.recent.SaveRecentSearchUseCase
 import javax.inject.Inject
 
 data class RecentSearchTermUseCase @Inject constructor(
     val getRecentSearchUseCase: GetRecentSearchUseCase,
     val saveRecentSearchUseCase: SaveRecentSearchUseCase,
-    val removeRecentSearchUseCase: RemoveRecentSearchUseCase
 )
--- TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/recent/SaveRecentSearchUseCase.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/domain/use_case/recent/SaveRecentSearchUseCase.kt
@@ -4,9 +4,23 @@ import com.example.tayapp.domain.repository.RecentSearchRepository
 import javax.inject.Inject
 
 class SaveRecentSearchUseCase @Inject constructor(private val repository: RecentSearchRepository) {
-    suspend operator fun invoke(query: String) {
-        repository.saveRecentSearchTerm(
-            query
-        )
+    suspend operator fun invoke(query: String, index: Int) {
+        val list = query.split(" ").toMutableList()
+        list.removeAt(index)
+        val saveSearchTerm = list.toString().replace("[\\[+,\\]]".toRegex(), "")
+        repository.saveRecentSearchTerm(saveSearchTerm)
+    }
+
+    suspend operator fun invoke(recentTerm: String, query: String) {
+        val list = recentTerm.split(" ").toMutableList()
+
+        if (list.contains(query)) {
+            list.remove(query)
+        }
+
+        val recentSearchString = list.toString().replace("[\\[+,\\]]".toRegex(), "")
+        val saveSearchTerm = "$query $recentSearchString".trim()
+
+        repository.saveRecentSearchTerm(saveSearchTerm)
     }
 }
