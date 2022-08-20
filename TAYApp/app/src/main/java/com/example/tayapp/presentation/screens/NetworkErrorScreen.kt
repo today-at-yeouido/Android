327b36b4ea1a10f898b7a265814f6ee7f13ddc94
Liv66 <78843258+Liv66@users.noreply.github.com>
2022-08-20 15:41:01 +0900

    style: NetworkError Screen (#150)

--- TAYApp/app/src/main/java/com/example/tayapp/presentation/screens/NetworkErrorScreen.kt
+++ TAYApp/app/src/main/java/com/example/tayapp/presentation/screens/NetworkErrorScreen.kt
@@ -1,34 +1,46 @@
 package com.example.tayapp.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tayapp.R
 import com.example.tayapp.presentation.states.UserState
 import com.example.tayapp.presentation.ui.theme.TayAppTheme
 
 @Composable
 fun NetworkErrorScreen(tryGetData: () -> Unit) {
     Box(
-        Modifier
-            .fillMaxSize()
-            .background(TayAppTheme.colors.background)
+        modifier = Modifier.fillMaxSize().clickable { tryGetData() }
     ) {
-        Text(
-            "네트워크 에러 !!",
-            fontSize = 30.sp,
-            color = TayAppTheme.colors.bodyText,
+        Image(
             modifier = Modifier
-                .align(Alignment.Center)
-                .clickable {
-                    UserState.network = true
-                    tryGetData()
-                }
+                .padding(top = 138.dp)
+                .size(100.dp)
+                .align(Alignment.TopCenter),
+            painter = painterResource(id = R.drawable.ic_tay_ch_emoji_gray_default),
+            contentDescription = "main_title_image"
+        )
+
+        Text(
+            text = "통신에 문제가 있어요.\n" +
+                    "네트워크를 확인해주세요!",
+            modifier = Modifier
+                .padding(top = 258.dp)
+                .align(Alignment.TopCenter),
+            textAlign = TextAlign.Center,
+            color = TayAppTheme.colors.disableText
         )
     }
 
