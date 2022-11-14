790644a40a9e5cedf553f75fc764bb7bee2a967c
GaheeKim <aqswefr@naver.com>
2022-11-14 23:27:45 +0900

    design: modify color value and mostviewed card color
    semantic color변경 및 핫한 법안 카드 색상 수정

--- TAYApp/app/src/main/java/com/todayeouido/tayapp/presentation/utils/StateColor.kt
+++ TAYApp/app/src/main/java/com/todayeouido/tayapp/presentation/utils/StateColor.kt
@@ -2,15 +2,15 @@ package com.todayeouido.tayapp.presentation.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.todayeouido.tayapp.presentation.ui.theme.TayAppTheme
+import com.todayeouido.tayapp.presentation.ui.theme.*
 
 @Composable
 fun StateColor(status: String): Color {
     when(status){
-        "소관위심사보고", "소관위심사", "본회의부의안건", "정부이송" -> return TayAppTheme.colors.caution2
-        "공포", "본회의의결" -> return TayAppTheme.colors.success2
-        "소관위접수", "접수" -> return TayAppTheme.colors.disableIcon
-        "철회", "대안반영폐기", "본회의불부의", "회송" -> return TayAppTheme.colors.danger2
-        else -> return TayAppTheme.colors.disableIcon
+        "소관위심사보고", "소관위심사", "본회의부의안건", "정부이송" -> return lm_semantic_yellow2
+        "공포", "본회의의결" -> return lm_semantic_green2
+        "소관위접수", "접수" -> return lm_gray300
+        "철회", "대안반영폐기", "본회의불부의", "회송" -> return lm_semantic_red2
+        else -> return lm_gray300
     }
 }
